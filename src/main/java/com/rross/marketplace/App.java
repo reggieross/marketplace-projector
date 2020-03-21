4aca6c8c417535d18329607cd5ae0485c0fe00b6
Reggie Ross <reggieross@Reggies-MacBook-Pro.local>
2020-03-20 21:55:08 -0600

    fixing app properties

--- src/main/java/com/rross/marketplace/App.java
+++ src/main/java/com/rross/marketplace/App.java
@@ -2,9 +2,20 @@ package com.rross.marketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class App {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String healthy() {
+        return "Up & Healthy";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(App.class);
     }
